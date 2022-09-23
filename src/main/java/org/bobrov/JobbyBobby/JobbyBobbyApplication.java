fb74ffc4fdf7b25c0e96152a136901152d517131
Anton Bobrov <anton.bobrov.98@gmail.com>
2022-09-23 18:40:30 +0300

    init commit

---
+++ src/main/java/org/bobrov/JobbyBobby/JobbyBobbyApplication.java
@@ -0,0 +1,13 @@
+package org.bobrov.JobbyBobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbyBobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbyBobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bobrov/JobbyBobby/JobbyBobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bobrov.JobbyBobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbyBobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
