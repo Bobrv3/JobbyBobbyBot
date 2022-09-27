09ee9be188175f0e3a8283a2985e97092e012a50
Anton Bobrov <anton.bobrov.98@gmail.com>
2022-09-27 18:36:09 +0300

    added reading vacancies from all pages

--- src/main/java/org/bobrov/JobbyBobby/service/HHclient.java
+++ src/main/java/org/bobrov/JobbyBobby/service/HHclient.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -22,12 +23,30 @@ public class HHclient {
 
     @SneakyThrows
     public List<Vacancy> findVacancies() {
-        ResponseEntity<HHresponse> exchange = restTemplate.exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
-        });
+        ResponseEntity<HHresponse> exchange = restTemplate.exchange(
+                URL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
 
         if (exchange.getStatusCode().equals(HttpStatus.OK)) {
             HHresponse hhResponse = exchange.getBody();
-            return hhResponse.getVacancies();
+            List<Vacancy> vacancies = hhResponse.getVacancies();
+
+            if (hhResponse.getPages() > 1) {
+                for (int i = 1; i < hhResponse.getPages(); i++) {
+                    exchange = restTemplate.exchange(
+                            String.format("%s&page=%s", URL, i),
+                            HttpMethod.GET,
+                            null,
+                            new ParameterizedTypeReference<>() {
+                            });
+                    Collections.addAll(vacancies, exchange.getBody().getVacancies().toArray(new Vacancy[0]));
+                }
+            }
+
+            return vacancies;
         } else {
             throw new HhResponseNotReceived("HhResponseNotReceived:" + exchange.getStatusCode() + " " + exchange.getStatusCodeValue());
         }
